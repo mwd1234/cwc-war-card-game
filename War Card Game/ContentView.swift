42024cd5124624ee33b1e26404e618cd82aa1902
Matthew Dembinski <matthewdembinski@gmail.com>
2024-01-23 22:51:56 -0700

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Matthew Dembinski on 1/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Matthew Dembinski on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
